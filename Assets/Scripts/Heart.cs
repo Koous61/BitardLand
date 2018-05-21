2ffb178fefbe1d5d0c078f095e62c873ee4f699b
Koous <dima_nichiporchik@mail.ru>
2018-05-21 23:19:19 +0300

    first commit

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heart : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Character character = collider.GetComponent<Character>();
+        
+        if (character)
+        {
+			if (character.Lives < 5) {
+				character.Lives++;
+				Destroy (gameObject);
+			}
+        }
+    }
+}
---
+++ Assets/Scripts/diespace.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class diespace : MonoBehaviour
+{
+    public GameObject respawn;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Unit unit = collider.GetComponent<Unit>();
+
+        if (unit && unit is Character)
+        {
+            unit.ReceiveDamage();
+            collider.transform.position = respawn.transform.position;
+        }
+    }
+}
